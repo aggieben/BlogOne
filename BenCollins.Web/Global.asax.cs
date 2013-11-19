b192e23131bbfec61eb63514b9c9811b9b2572f6
Benjamin Collins <ben.collins@acm.org>
2013-11-19 15:30:59 -0600

    add miniprofiler; remove bootstrap sources; update Css for sidebar; support mobile

--- BenCollins.Web/Global.asax.cs
+++ BenCollins.Web/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using StackExchange.Profiling;
 
 namespace BenCollins.Web
 {
@@ -16,6 +17,19 @@ namespace BenCollins.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_BeginRequest()
+        {
+            if (Request.IsLocal)
+            {
+                MiniProfiler.Start();
+            }
+        }
+
+        protected void Application_EndRequest()
+        {
+            MiniProfiler.Stop();
         }
     }
 }
