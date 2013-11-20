d97cf24a984a06f322a323310e4b0fa3d2767b57
Benjamin Collins <ben.collins@acm.org>
2013-11-19 22:31:12 -0600

    add repository interface

---
+++ BenCollins.Web/Data/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace BenCollins.Web.Data
+{
+    interface IRepository<T> where T : class
+    {
+        void Add(T item);
+        void Remove(T item);
+        void Update(T item);
+        T FindBySid(Guid id);
+        T FindById(int id);
+        IEnumerable<T> Find(Expression<Func<T, bool>> predicate);
+        IEnumerable<T> FindAll();
+    }
+}
