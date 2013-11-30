6ca770f05b7bd3594c53afc6c054c04125f3ff4c
Benjamin Collins <ben.collins@acm.org>
2013-11-30 02:37:23 -0600

    tweak db schema to get it working with ASP.NET Identity and dapper

--- BenCollins.Database/dbo/Tables/ExternalLogins.sql
+++ BenCollins.Database/dbo/Tables/ExternalLogins.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [dbo].[ExternalLogins]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Sid] UNIQUEIDENTIFIER NOT NULL DEFAULT newsequentialid(), 
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Sid] UNIQUEIDENTIFIER NULL DEFAULT newsequentialid(), 
     [DefaultUserName] NVARCHAR(128) NOT NULL, 
-    [ProviderName] NVARCHAR(128) NOT NULL, 
+    [LoginProvider] NVARCHAR(128) NOT NULL, 
     [ProviderKey] NVARCHAR(1024) NOT NULL
 )
