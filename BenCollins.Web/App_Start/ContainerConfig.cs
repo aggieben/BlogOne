0cb89e3d0ffdd31b1c7ebd7dd166a0eed3c9ed9d
Benjamin Collins <ben.collins@acm.org>
2013-11-30 01:44:11 -0600

    wire up Munq using a submodule instead of nuget

--- BenCollins.Web/App_Start/ContainerConfig.cs
+++ BenCollins.Web/App_Start/ContainerConfig.cs
@@ -1,23 +1,34 @@
+using System.Web.Configuration;
 using System.Web.Mvc;
-using Munq.MVC3;
+using BenCollins.Web.Controllers;
 using BenCollins.Web.Data;
+using Munq;
 
 namespace BenCollins.Web {
 	public static class ContainerConfig {
 		public static void RegisterContainer() 
         {
-			DependencyResolver.SetResolver(new MunqDependencyResolver());
-
-            RegisterDependencies();
+			DependencyResolver.SetResolver(new Mvc.DependencyResolver(BuildContainer()));
 		}
 
-        public static void RegisterDependencies()
+        public static IocContainer BuildContainer()
         {
-            var ioc = MunqDependencyResolver.Container;
+            var ioc = new IocContainer();
 
+            ioc.Register<IDbConnectionFactory>(c => new SqlConnectionFactory(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString));
+
+            // repositories
             ioc.Register<IExternalLoginRepository, ExternalLoginRepository>();
+
+            // controllers
+            ioc.Register<AdminController>(
+                c =>
+                {
+                    var xlr = c.Resolve(typeof(IExternalLoginRepository)) as IExternalLoginRepository;
+                    return new AdminController(xlr);
+                });
+
+            return ioc;
         }
 	}
 }
- 
-
--- BenCollins.Web/Global.asax.cs
+++ BenCollins.Web/Global.asax.cs
@@ -6,7 +6,6 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using StackExchange.Profiling;
-using BenCollins.Web.Mvc;
 
 namespace BenCollins.Web
 {
