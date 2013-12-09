4d7da397146ea8948a205698bd432218468a04ac
Benjamin Collins <ben.collins@acm.org>
2013-12-09 03:30:19 -0600

    update tables for SSDT compare syntax, as well as ad slug support to Posts.  #4

--- BenCollins.Database/dbo/Tables/ExternalLogins.sql
+++ BenCollins.Database/dbo/Tables/ExternalLogins.sql
@@ -1,9 +1,9 @@
 ﻿CREATE TABLE [dbo].[ExternalLogins]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [Sid] UNIQUEIDENTIFIER NOT NULL DEFAULT newid(), 
+    [Sid] UNIQUEIDENTIFIER NOT NULL DEFAULT (newid()), 
     [DefaultUserName] NVARCHAR(128) NOT NULL, 
     [LoginProvider] NVARCHAR(128) NOT NULL, 
     [ProviderKey] NVARCHAR(1024) NOT NULL, 
-    [CreationDate] DATETIME NOT NULL DEFAULT getutcdate()
+    [CreationDate] DATETIME NOT NULL DEFAULT (getutcdate())
 )
--- BenCollins.Database/dbo/Tables/Posts.sql
+++ BenCollins.Database/dbo/Tables/Posts.sql
@@ -1,8 +1,14 @@
 ﻿CREATE TABLE [dbo].[Posts]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [Sid] UNIQUEIDENTIFIER NOT NULL DEFAULT newid(), 
-    [CreationDate] DATETIME NOT NULL DEFAULT getutcdate(), 
+    [Sid] UNIQUEIDENTIFIER NOT NULL DEFAULT (newid()), 
+    [CreationDate] DATETIME NOT NULL DEFAULT (getutcdate()), 
     [Title] NVARCHAR(256) NOT NULL, 
-    [Body] TEXT NOT NULL
+    [Body] TEXT NOT NULL, 
+    [Slug] NVARCHAR(50) NOT NULL, 
+    [SlugHash] AS (checksum(Slug)) PERSISTED NOT NULL
 )
+
+GO
+
+CREATE INDEX [IX_Posts_SlugHash] ON [dbo].[Posts] ([SlugHash])
