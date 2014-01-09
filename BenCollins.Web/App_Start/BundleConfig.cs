55eb0a7a89ad228a55c87f544fa280619e14ded2
Benjamin Collins <ben.collins@acm.org>
2014-01-08 23:24:57 -0600

    add grandejs files to project and bundles.  also comment out rest of css to see style/fonts of grandjs without interference

--- BenCollins.Web/App_Start/BundleConfig.cs
+++ BenCollins.Web/App_Start/BundleConfig.cs
@@ -23,9 +23,12 @@ namespace BenCollins.Web
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      //"~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          //"~/Content/bootstrap.css",
+            //          "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/grandejs").Include(
+                      "~/Content/editor.css",
+                      "~/Content/menu.css"));
         }
     }
 }
