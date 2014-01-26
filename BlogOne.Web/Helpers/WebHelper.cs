6d539c41cbe44591d670abff575a6b39a67194b8
Benjamin Collins <ben.collins@acm.org>
2014-01-25 22:14:50 -0600

    generate random shortcodes

--- BlogOne.Web/Helpers/WebHelper.cs
+++ BlogOne.Web/Helpers/WebHelper.cs
@@ -8,9 +8,22 @@ namespace BlogOne.Web.Helpers
 {
     public class WebHelper
     {
-        private static Dictionary<string, string> _cachedPathInputs;
+        private static readonly Dictionary<string, string> _cachedPathInputs = new Dictionary<string, string>();
 
-        public static string GetRandomizedUriComponent(string input)
+        public static string GetShortcode(int len, string input = "a-zA-Z0-9")
+        {
+            var expandedInput = ExpandShortcodeInput(input);
+            var gen = new Random(DateTime.UtcNow.Millisecond);
+            var sb = new StringBuilder(len);
+            while (sb.Length < len)
+            {
+                var index = gen.Next(expandedInput.Length - 1);
+                sb.Append(expandedInput[index]);
+            }
+            return sb.ToString();
+        }
+
+        private static string ExpandShortcodeInput(string input)
         {
             if (_cachedPathInputs.ContainsKey(input))
             {
@@ -18,31 +31,31 @@ namespace BlogOne.Web.Helpers
             }
 
             var needsGeneration = false;
-            StringBuilder sb = new StringBuilder();
-            foreach (char c in input)
+            var sb = new StringBuilder();
+            foreach (var c in input)
             {
                 if (c != '-')
-	            {
-		            if (needsGeneration)
-		            {
-			            for (var ci = (char)(sb[sb.Length-1]+1); ci <= c; ci = (char)(ci+1))
-			            {
-				            sb.Append(ci);
-			            }
-			            needsGeneration = false;
-		            }
-		            else
-		            {
-			            sb.Append(c);
-		            }
-	            }
-	            else 
-	            {
-		            if (needsGeneration) throw new FormatException(String.Format("Unexpected '-'; [{0}]", sb));
-		            needsGeneration = true;
-	            }
+                {
+                    if (needsGeneration)
+                    {
+                        for (var ci = (char)(sb[sb.Length - 1] + 1); ci <= c; ci = (char)(ci + 1))
+                        {
+                            sb.Append(ci);
+                        }
+                        needsGeneration = false;
+                    }
+                    else
+                    {
+                        sb.Append(c);
+                    }
+                }
+                else
+                {
+                    if (needsGeneration) throw new FormatException(String.Format("Unexpected '-'; [{0}]", sb));
+                    needsGeneration = true;
+                }
             }
-            return _cachedPathInputs[input] = sb.ToString();            
+            return _cachedPathInputs[input] = sb.ToString();
         }
     }
 }
