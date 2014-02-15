d7e67c4513c39d538e9c2273d2625065f38d25c4
Benjamin Collins <ben.collins@acm.org>
2014-02-14 23:24:00 -0600

    fix auth redirect

--- BlogOne.Web/App_Start/Startup.Auth.cs
+++ BlogOne.Web/App_Start/Startup.Auth.cs
@@ -14,7 +14,7 @@ namespace BlogOne.Web
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
                 AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
-                LoginPath = new PathString("/Account/Login")
+                LoginPath = new PathString("/admin/login")
             });
             // Use a cookie to temporarily store information about a user logging in with a third party login provider
             app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
