cddaa11d1426389a75012218d5bafcc25afe2c61
Benjamin Collins <ben.collins@acm.org>
2014-06-23 23:06:57 -0500

    progress commit.  no idea what I was doing at the time.

--- BlogOne.Web/Scripts/xmedium.js
+++ BlogOne.Web/Scripts/xmedium.js
@@ -15,7 +15,36 @@ XMedium = (function() {
       };
       return pop = new Popover({
         position: 'selection',
-        content: "<input type=\"url\" size=\"35\" autofocus=\"true\" placeholder=\"paste or type a link\"/>",
+        content: "<input type=\"url\" size=\"35\" autofocus=\"true\" placeholder=\"paste or type a url\"/>",
+        eventing: [
+          {
+            selector: 'input',
+            event: 'keyup',
+            filter: eventFilter,
+            handler: eventHandler
+          }
+        ]
+      });
+    },
+    codefmt: function(e) {
+      var code, pre, range;
+      range = window.getSelection().getRangeAt(0);
+      pre = document.createElement('pre');
+      code = document.createElement('code');
+      range.surroundContents(code);
+      return pre.appendChild(code.parentNode.replaceChild(pre, code));
+    },
+    gistins: function(e) {
+      var eventFilter, eventHandler, pop;
+      eventFilter = function(e) {
+        return ((e.key != null) && e.key === 'enter') || ((e.which != null) && e.which === 13);
+      };
+      eventHandler = function(e) {
+        return console.log('submit gist');
+      };
+      return pop = new Popover({
+        postion: 'selection',
+        content: "<input size=\"35\" autofocus=\"true\" placeholder=\"paste or type a gist id or url\"/>",
         eventing: [
           {
             selector: 'input',
