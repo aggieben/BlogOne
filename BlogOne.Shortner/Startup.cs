e4e5acad4028dc6f00b7103b664898c0dc25299e
Ben Collins <ben.collins@acm.org>
2014-08-01 18:46:23 -0500

    add OOB MVC project for shortener

---
+++ BlogOne.Shortner/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BlogOne.Shortner.Startup))]
+namespace BlogOne.Shortner
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
