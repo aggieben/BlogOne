44bdbfbdcf05c6e770936f7a4ef0b575ea79f891
Benjamin Collins <ben.collins@acm.org>
2014-08-02 01:31:38 -0500

    add ShortUrl model/table

---
+++ BlogOne.Database/dbo/Tables/ShortUrls.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Table1]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Sid] UNIQUEIDENTIFIER NOT NULL DEFAULT (newid()), 
+    [CreationDate] DATETIME NOT NULL DEFAULT (getutcdate()), 
+    [Enabled] BIT NOT NULL DEFAULT 1, 
+    [Url] NVARCHAR(MAX) NOT NULL, 
+    [ShortCode] NVARCHAR(10) NOT NULL
+)
