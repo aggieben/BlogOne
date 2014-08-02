8c73e1c2b960abd6cc560a931df9a4fc160aeaac
Benjamin Collins <ben.collins@acm.org>
2014-08-02 00:37:53 -0500

    add StackExchange.Redis and a Common project

---
+++ BlogOne.Common/Web/Mvc/DependencyResolver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace BlogOne.Common.Web.Mvc
+{
+    public class DependencyResolver : IDependencyResolver
+    {
+        private readonly Munq.IocContainer _container;
+
+        public DependencyResolver(Munq.IocContainer container)
+        {
+            _container = container;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return _container.CanResolve(serviceType) ? _container.Resolve(serviceType) : null;
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return _container.CanResolve(serviceType) ? _container.ResolveAll(serviceType) : new object[] { };
+        }
+    }
+}
