f22226f35ad5932c282d125b5c2998f8a5906241
Benjamin Collins <ben.collins@acm.org>
2014-08-03 01:00:09 -0500

    rename Models => Model and add ShortUrl model

---
+++ BlogOne.Shortner/Model/ModelBase.cs
@@ -0,0 +1,18 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+
+namespace BlogOne.Shortener.Model
+{
+    public abstract class ModelBase
+    {
+        [Key]
+        public int? Id { get; set; }
+        [Computed]
+        public Guid? Sid { get; set; }
+        [Computed]
+        public DateTime CreationDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+
+        internal bool Dirty { get; set; }
+    }
+}
---
+++ BlogOne.Shortner/Model/ShortUrl.cs
@@ -0,0 +1,11 @@
+﻿using BlogOne.Shortener.Model;
+
+namespace BlogOne.Shortner.Model
+{
+    public class ShortUrl : ModelBase
+    {
+        public bool Enabled { get; set; }
+        public string Url { get; set; }
+        public string ShortCode { get; set; }
+    }
+}
