9a4cc7f7bf863d6e3b8629690494fa49f07ecc27
Benjamin Collins <ben.collins@acm.org>
2014-08-04 10:09:22 -0500

    wire up miniprofiler

--- BlogOne.Shortner/Global.asax.cs
+++ BlogOne.Shortner/Global.asax.cs
@@ -1,6 +1,10 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using BlogOne.Shortner.App_Start;
+using StackExchange.Profiling;
+using StackExchange.Profiling.Mvc;
 
 namespace BlogOne.Shortner
 {
@@ -13,6 +17,23 @@ namespace BlogOne.Shortner
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             ContainerConfig.RegisterContainer();
+
+            var engines = ViewEngines.Engines.ToList();
+            ViewEngines.Engines.Clear();
+            foreach (var ve in engines)
+            {
+                ViewEngines.Engines.Add(new ProfilingViewEngine(ve));
+            }
+        }
+
+        protected void Application_BeginRequest()
+        {
+            MiniProfiler.Start();
+        }
+
+        protected void Application_EndRequest()
+        {
+            MiniProfiler.Stop();
         }
     }
 }
