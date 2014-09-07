a8f6320f92c5fc528b5d6d0a6792567468815766
Ben Collins <aggieben@gmail.com>
2014-09-07 14:43:43 +0000

    basic outline of new app

--- BlogOne.Shortener/Startup.cs
+++ BlogOne.Shortener/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.AspNet.Builder;
+using Microsoft.Framework.DependencyInjection;
 
 namespace BlogOne.Shortener
 {
@@ -7,7 +8,12 @@ namespace BlogOne.Shortener
     {
         public void Configure(IBuilder app)
         {
-            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+            app.UseServices(services =>
+            {
+                services.AddMvc();
+            });
+
+            app.UseMvc();
         }
     }
 }
