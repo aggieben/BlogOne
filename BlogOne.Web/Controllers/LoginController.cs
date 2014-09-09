0deacba88f2681ce04c26491d0eeac609a5c03af
Ben Collins <aggieben@gmail.com>
2014-09-09 00:20:11 -0500

    drop back to alpha3 and add a logincontroller (which will have to wait until alpha4)

---
+++ BlogOne.Web/Controllers/LoginController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace BlogOne.Web.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- BlogOne.Web/Startup.cs
+++ BlogOne.Web/Startup.cs
@@ -1,6 +1,9 @@
-﻿using System;
-using Microsoft.AspNet.Builder;
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Routing;
+//using Microsoft.AspNet.Security.Cookies;
+using Microsoft.AspNet.Http;
 using Microsoft.Framework.DependencyInjection;
+//using Microsoft.AspNet.Security.Google;
 
 namespace BlogOne.Web
 {
@@ -8,12 +11,38 @@ namespace BlogOne.Web
     {
         public void Configure(IBuilder app)
         {
+            ConfigureServices(app);
+            ConfigureRoutes(app);
+        }
+
+        public void ConfigureServices(IBuilder app)
+        {
             app.UseServices(services =>
             {
                 services.AddMvc();
             });
+        }
 
-            app.UseMvc();
+        public void ConfigureRoutes(IBuilder app)
+        {
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute("Default", "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+        public void ConfigureAuth(IBuilder app)
+        {
+            //app.UseCookieAuthentication(new CookieAuthenticationOptions
+            //{                
+            //    LoginPath = new PathString("/login"),
+            //    LogoutPath = new PathString("/logout")
+            //});
+
+            //var googleOptions = new GoogleAuthenticationOptions
+            //{
+
+            //};
         }
     }
 }
